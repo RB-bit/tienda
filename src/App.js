3104d2cb56d93369439e36fed16870d4376d5e96
RB-bit <rodrigobossi097@gmail.com>
2021-04-24 19:12:30 -0300

    intall react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import NavBarComponent from './components/navbar/NavBarComponent'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+import itemListContainer from './containers/itemListContainer';
 
 function App() {
   return (
     <React.Fragment>
-      <NavBarComponent />
+      <BrowserRouter>
+        <NavBarComponent />
+        <Route>
+          <itemListContainer />
+        </Route>
+      </BrowserRouter>
     </React.Fragment>
   );
 }
--- src/containers/itemListContainer.jsx
+++ src/containers/itemListContainer.jsx
@@ -1,7 +1,7 @@
 const itemListContainer = () => {
     return (
         <div>
-            
+            Hola
         </div>
     )
 }
