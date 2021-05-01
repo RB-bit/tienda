fc918d300c0d38b9a3f4065cfffce8528f33e7bb
RB-bit <rodrigobossi097@gmail.com>
2021-05-01 12:23:49 -0300

    Add Data Base

--- src/containers/items/ItemListContainer.js
+++ src/containers/items/ItemListContainer.js
@@ -1,97 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import ItemList from './ItemList'
+import DataBase from '../../DataBase/DataBase.json'
 
 const ItemListContainer = () => {
 
     const [Items, setItems] = useState([])
 
     useEffect(()=>{
-        const productos = [
-            {
-                "precio": 500,
-                "id": 1,
-                "title": "Nike React 270 Black",
-                "pictureUrl": "./assets/products/react-270-1.png",
-                "stock": 3,
-            },
-            {
-                "precio": 300,
-                "id": 2,
-                "title": "Nike React 270 Bauhaus",
-                "pictureUrl": "./assets/products/react-270-2.png",
-                "stock": 4,
-            },
-            {
-                "precio": 100,
-                "id": 3,
-                "title": "Nike React 270 Black Agua",
-                "pictureUrl": "./assets/products/react-270-3.png",
-                "stock": 6,
-            },
-            {
-                "precio": 50,
-                "id": 4,
-                "title": "Nike React 270 White",
-                "pictureUrl": "./assets/products/react-270-4.png",
-                "stock": 9,
-            },
-            {
-                "precio": 100,
-                "id": 5,
-                "title": "Nike React 270 Reggae",
-                "pictureUrl": "./assets/products/react-270-5.png",
-                "stock": 2,
-            },
-            {
-                "precio": 150,
-                "id": 6,
-                "title": "Nike React 270 Pnk",
-                "pictureUrl": "./assets/products/react-270-6.png",
-                "stock": 5,
-            },
-            {
-                "precio": 150,
-                "id": 7,
-                "title": "Nike React 270 Pnk",
-                "pictureUrl": "./assets/products/react-270-6.png",
-                "stock": 5,
-            },
-            {
-                "precio": 150,
-                "id": 8,
-                "title": "Nike React 270 Pnk",
-                "pictureUrl": "./assets/products/react-270-6.png",
-                "stock": 5,
-            },
-            {
-                "precio": 150,
-                "id": 9,
-                "title": "Nike React 270 Pnk",
-                "pictureUrl": "./assets/products/react-270-6.png",
-                "stock": 5,
-            },
-            {
-                "precio": 150,
-                "id": 10,
-                "title": "Nike React 270 Pnk",
-                "pictureUrl": "./assets/products/react-270-6.png",
-                "stock": 5,
-            },
-            {
-                "precio": 150,
-                "id": 11,
-                "title": "Nike React 270 Pnk",
-                "pictureUrl": "./assets/products/react-270-6.png",
-                "stock": 5,
-            },
-            {
-                "precio": 150,
-                "id": 12,
-                "title": "Nike React 270 Pnk",
-                "pictureUrl": "./assets/products/react-270-6.png",
-                "stock": 5,
-            }
-                            ]
+        const productos = DataBase
+        
         const task = new Promise((resolve, reject) => {
             setTimeout(() =>{
                 setItems(productos)
